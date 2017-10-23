77b698111d79f92071c50d39af082ee8a6a0aa96
Shumei Huang <rose1264@gmail.com>
2017-10-23 23:17:32 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,6 +10,14 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
+def position_taken? (board, index)
+ if board[index] == nil || board[index] == "" || board[index] ==" "
+   return false
+ else
+   return true
+ end
+end
+
 def valid_move?(board, index)
   if !position_taken?(board, index) && index.between?(0, 8)
      return true
